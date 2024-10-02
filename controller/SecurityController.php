b37b7b0db9855dda45dee33f54d9171b80e12ac2
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-10-02 15:51:44 +0200

    beginning of function login and register

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -3,11 +3,50 @@ namespace Controller;
 
 use App\AbstractController;
 use App\ControllerInterface;
+use App\Manager;
+use App\Session;
+use Model\Managers\UserManager;
 
 class SecurityController extends AbstractController{
     // contiendra les méthodes liées à l'authentification : register, login et logout
 
-    public function register () {}
-    public function login () {}
+    public function register () {
+        if(isset($_POST["submit"])) {
+            $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
+            $pass1 = filter_input(INPUT_POST, "pass1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $pass2 = filter_input(INPUT_POST, "pass2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            if($pseudo && $email && $pass1 && $pass2) {
+                $userManager = new UserManager();
+
+                if($pass1==$pass2 && strlen($pass1)>= 5) {
+                    $userManager->add([
+                        "pseudo" => $pseudo,
+                        "email" => $email,
+                        "password" => password_hash($pass1, PASSWORD_DEFAULT)
+                    ]);
+                    $this->redirectTo("security", "login");
+                }
+            }
+        }
+
+        return [
+            "view" => VIEW_DIR."security/register.php",
+            "meta_description"=>"S'inscrire"
+
+        ];
+    }
+    public function login () {
+        if(isset($_POST["submit"])) {
+            $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
+            $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            if($pseudo && $email) {
+                $userManager = new UserManager();
+            }
+            
+    }
+}
     public function logout () {}
 }
---
+++ view/security/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php?ctrl=security&action=login" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email"><br>
+
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password">
+
+        <input type="submit" name="submit" value="Se connecter">
+    </form>
+</body>
+</html>
