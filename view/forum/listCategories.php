06183d0ed63948c8058e0687258b9f233df25f30
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-10-08 16:44:03 +0200

    css homepage + beginning css of the list topics

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -1,7 +1,7 @@
 <?php
     $categories = $result["data"]['categories']; 
 ?>
-
+<div id="wrapper-category">
 <h1>Liste des catégories</h1>
 
 <?php
@@ -10,6 +10,6 @@ foreach($categories as $category ){
     <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><?= $category->getNomCategory() ?></a></p>
     
 <?php }
-
-
+?>
+</div>
   
--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -6,42 +6,54 @@
     $role = App\Session::getUser() ? App\Session::getUser()->getRole() : null; 
     $ban = App\Session::getUser() ? App\Session::getUser()->getBan() : null; 
 ?>
-
-<h1>Liste des topics</h1>
-
-<?php 
-    if ($topics) {
-        foreach($topics as $topic ){ 
-            if($userId && ($topic->getUser()->getId() == $userId || $role == "ROLE_ADMIN")) {
-            ?> <p><a href="index.php?ctrl=forum&action=deleteTopic&id=<?= $topic->getId() ?>">Supprimer le topic</a></p> <?php 
-            { if ($topic->getLocked()===0) {?>
-            <p><a href="index.php?ctrl=forum&action=lockTopic&id=<?= $topic->getId() ?>">Verrouiller le topic</a></p>
-            <?php } if ($topic->getLocked()===1) { ?>
-            <p><a href="index.php?ctrl=forum&action=unlockedTopic&id=<?= $topic->getId() ?>">Déverouiller le topic</a></p>
-            
-        <?php } 
-        } 
-    }  ?>
-    <p><a href="index.php?ctrl=forum&action=listPostsByTopics&id=<?= $topic->getId() ?>"><?= $topic->getTitre() ?> </a> par <?= $topic->getUser() ?> (<?= $topic->formatDateTopic() ?>)</p>
-<?php } ?>
-     
-<?php } else {
-    echo "Pas de topics";
-}
-
-?>
-
-<?php  if ($ban === 1) {
-    echo "Vous êtes banni";
-} else if($userId) { 
-     ?>
+<div id="wrapper-topic">
+    <h1>Liste des topics</h1>
+    
+    <div class="topics">
+    <?php 
    
-<form action="index.php?ctrl=forum&action=addTopic&id=<?=$category->getId() ?>" method="POST">
-    <label for="titre">Titre:</label><br>
-    <input type="titre" name="titre"><br>
-    <textarea name="texte" id="texte" rows="4" cols="50">
-    </textarea>
-    <input type="submit" value="submit">
-</form>
-<?php } 
-?>
+        if ($topics) {
+            foreach($topics as $topic ){ 
+                if($userId && ($topic->getUser()->getId() == $userId || $role == "ROLE_ADMIN")) {
+                    ?> 
+                  
+                        <div class="delete-btn">
+                            <p><a href="index.php?ctrl=forum&action=deleteTopic&id=<?= $topic->getId() ?>">Supprimer le topic</a></p> 
+                        </div> 
+                        <?php 
+                        { if ($topic->getLocked()===0) {?>
+                        <div class="lock-btn">
+                            <p><a href="index.php?ctrl=forum&action=lockTopic&id=<?= $topic->getId() ?>">Verrouiller le topic</a></p>
+                        </div>
+                        <?php } if ($topic->getLocked()===1) { ?>
+                        <p><a href="index.php?ctrl=forum&action=unlockedTopic&id=<?= $topic->getId() ?>">Déverouiller le topic</a></p>
+                 
+            <?php } 
+            } 
+        }  ?>
+        <p><a href="index.php?ctrl=forum&action=listPostsByTopics&id=<?= $topic->getId() ?>"><?= $topic->getTitre() ?> </a> par <?= $topic->getUser() ?> (<?= $topic->formatDateTopic() ?>)</p>
+    <?php } ?>
+        
+    <?php } else {
+        echo "Pas de topics";
+    }
+
+    ?>
+    </div>
+
+    <?php  if ($ban === 1) {
+        echo "Vous êtes banni";
+    } else if($userId) { 
+        ?>
+    <div class="topic-form">
+        <form action="index.php?ctrl=forum&action=addTopic&id=<?=$category->getId() ?>" method="POST">
+            <label for="titre">Titre:</label><br>
+            <input type="titre" name="titre"><br>
+            <textarea name="texte" id="texte" rows="4" cols="50">
+            </textarea>
+            <input type="submit" value="submit">
+        </form>
+    </div>
+    <?php } 
+    ?>
+</div>
