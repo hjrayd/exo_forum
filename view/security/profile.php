99d87293ce8b41321a91d6f0d3a5b42ab6b7dd1f
Hajar <hajarayaddouz29@gmail.com>
2024-10-11 09:22:11 +0200

    profile css

--- view/security/profile.php
+++ view/security/profile.php
@@ -4,20 +4,27 @@
       //var_dump($topics);die;
 ?>
 
-Mon profil: <br>
-    Pseudo :  <?=  App\Session::getUser()->getPseudo() ?> <br>
-    Date d'inscription :  <?=  App\Session::getUser()->formatDateInscription() ?> <br>
+<div id="wrapper-profile">
 
-Mes topics: <br>
+    <h1> Mon profil: </h1> <br>
+    <div class="infos-profile">
+        Pseudo :  <?=  App\Session::getUser()->getPseudo() ?> <br>
+        Date d'inscription :  <?=  App\Session::getUser()->formatDateInscription() ?> <br>
+    </div>
+    <div class="topic-profile">
+        <h2>Mes topics: </h2><br>
 
-   <?php if($topics) {
-      
-        foreach ($topics as $topic) { ?>
-        <ul>
-            <li> <?= $topic->getTitre() ?> </li>
-        </ul>
-         <?php } }?>
-
-<a href="index.php?ctrl=security&action=deleteProfile&id=<?= $user->getId() ?>">Supprimer mon profil</a></p> <?php
-        //var_dump($topics);die;
-    ?>
+        <?php if($topics) {
+            
+                foreach ($topics as $topic) { ?>
+                <ul>
+                    <li> <a href="index.php?ctrl=forum&action=listPostsByTopics&id=<?= $topic->getId() ?>"><?=$topic->getTitre() ?></a> <?php ?> </li>
+                </ul>
+                <?php } }?>
+    </div>
+    <div class="delete-profile">
+    <a href="index.php?ctrl=security&action=deleteProfile&id=<?= $user->getId() ?>">Supprimer mon profil</a></p> <?php
+            //var_dump($topics);die;
+        ?>
+    </div>
+</div>
