549db7bbe85eb05c78bc7ad5cc1cd4f2eb64f6e2
Hajar <hajarayaddouz29@gmail.com>
2024-10-11 09:55:17 +0200

    the entire website is responsive

--- view/layout.php
+++ view/layout.php
@@ -20,9 +20,11 @@
                     <nav>
                         <div id="nav-left">
                             <a href="index.php?ctrl=home">Accueil</a>
+                            
                             <?php
                             if(App\Session::isAdmin()){
                                 ?>
+                                 
                                 <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a>
                             <?php } ?>
                         </div>
@@ -31,8 +33,10 @@
                             // si l'utilisateur est connecté 
                             if(App\Session::getUser()){
                                 ?>
+                                <a href="index.php?ctrl=forum&action=index">Catégories</a>
                                 <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
                                 <a href="index.php?ctrl=security&action=profile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
+                               
                                 <?php
                             }
                             else{
